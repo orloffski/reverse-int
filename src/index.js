6e4cf1a6c985ca987be19daca6c12c3b781b5cc3
Evgen Orlovski <orloffski@gmail.com>
2023-01-13 14:46:34 +0300

    feat: implement reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function reverse (n) {
-  
+  let newNumber = 0;
+  let tmp = Math.abs(n);
+
+  while( tmp >= 10 ){
+	newNumber += tmp % 10;
+
+	tmp = (tmp - tmp % 10) / 10;
+
+	newNumber *= 10;
+  }
+
+  newNumber += tmp;
+
+  return newNumber;
+
 }
